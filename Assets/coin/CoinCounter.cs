5fbca733f1348095e976b7f76c7ea246fad8fcb4
Wehad44 <159052565+Wehad44@users.noreply.github.com>
2026-09-13 20:45:12 +0300

    Clean up CoinCounter.cs by removing comments
    Removed commented-out code for the CoinCounter class.

--- Assets/coin/CoinCounter.cs
+++ Assets/coin/CoinCounter.cs
@@ -1,32 +1,4 @@
-/*using UnityEngine;
-using UnityEngine.UI;
 
-public class CoinCounter : MonoBehaviour
-{
-    public static int coinCount = 0; 
-    public Text coinText;
-
-    private void Start()
-    {
-        coinCount = 0;
-        UpdateUI();
-    }
-
-
-    public void AddCoin()
-    {
-        coinCount++;
-        UpdateUI();
-        PlayerPrefs.SetInt("Coins", coinCount);
-    }// add
-
-
-    private void UpdateUI()
-    {
-        coinText.text = "Coins: " + coinCount.ToString();
-    }
-}
-*/
 
 using UnityEngine;
 using UnityEngine.UI;
